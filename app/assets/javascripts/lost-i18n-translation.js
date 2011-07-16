598709446483d34db837201e66bc3ad215670cbf
Andrea <andrea.damico@gmail.com>
2011-07-16 17:20:40 +0200

    css tweaks: separate labels from counts in translation bar

--- app/assets/javascripts/lost-i18n-translation.js
+++ app/assets/javascripts/lost-i18n-translation.js
@@ -4,11 +4,10 @@ $(document).ready(function() {
 
   var $tr_missing = $('<span id="lost-i18n-translation-tr-missing-count" />');
   $tr_missing.append( $translation_missing_count );
-  $('#missing_label').append($tr_missing);
+  $('#lost-i18n-translation-tr-missing').append($tr_missing);
 
   var $tr_present = $('<span id="lost-i18n-translation-tr-present-count" />');
   $tr_present.append( $translation_present_count );
-  $('#present_label').append($tr_present);
-
+  $('#lost-i18n-translation-tr-present').append($tr_present);
 });
 
--- lib/lost-i18n-translation/helper.rb
+++ lib/lost-i18n-translation/helper.rb
@@ -2,8 +2,13 @@ module LostI18nTranslation
   module Helper
     def translation_bar
       content_tag :div, :id => "lost-i18n-translation-div" do
-        safe_concat "<span id='missing_label'>" + t_old('lost_i18n_translation.missing_label') + "</span>"
-        safe_concat "<span id='present_label'>" + t_old('lost_i18n_translation.present_label') + "</span>"
+        safe_concat "<span id='lost-i18n-translation-tr-missing'>"
+        safe_concat "<span id='lost-i18n-translation-tr-missing-label'>" + t_old('lost_i18n_translation.missing_label') + "</span>"
+        safe_concat "</span>"
+        safe_concat " | "
+        safe_concat "<span id='lost-i18n-translation-tr-present'>"
+        safe_concat "<span id='lost-i18n-translation-tr-present-label'>" + t_old('lost_i18n_translation.present_label') + "</span>"
+        safe_concat "</span>"
       end
     end
   end
